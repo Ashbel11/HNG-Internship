ef33dd33895960c88740d71736f9c65bead7557d
Adeniji Joshua Adewale <adenijiwale17@gmail.com>
2026-04-29 18:23:45 +0100

    fix: guess

--- Task-3/Backend/main.py
+++ Task-3/Backend/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import JSONResponse
 from fastapi.exceptions import RequestValidationError
+from fastapi.staticfiles import StaticFiles
 import os
 
 from middleware.version import APIVersionMiddleware
